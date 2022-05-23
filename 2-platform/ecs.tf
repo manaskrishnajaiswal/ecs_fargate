d88a34093497a9b70d29e12d869fe95102cc827e
manas5996 <manas5996@gmail.com>
2022-05-23 21:44:07 +0530

    code change commit

--- 2-platform/ecs.tf
+++ 2-platform/ecs.tf
@@ -33,6 +33,19 @@ resource "aws_alb" "ecs_cluster_alb" {
     }
 }
 
+resource "aws_route53_record" "ecs_load_balancer_record" {
+    name = "*.${var.ecs_domain_name}"
+    type = "A"
+    zone_id = data.aws_route53_zone.ecs_domain.zone_id
+
+    alias {
+        evaluate_target_health = false
+        name = aws_alb.ecs_cluster_alb.dns_name
+        zone_id = aws_alb.ecs_cluster_alb.zone_id      
+    }
+  
+}
+
 
 
 
